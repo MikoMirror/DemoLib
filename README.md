ca6555fc12245f51979f1f72a127f522cfdb079f
MikoMIm <m.mordvinow@gmail.com>
2024-06-14 22:02:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutterlibdata
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutterlibdata/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutterlibdata
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
