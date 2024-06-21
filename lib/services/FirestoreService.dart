0cd5273b60bbf0f05f5e6d741145fbbcf3833f69
MikoMIm <m.mordvinow@gmail.com>
2024-06-21 23:57:11 +0200

    added functions to delete collections and books

--- lib/services/FirestoreService.dart
+++ lib/services/FirestoreService.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import '/models/book.dart'; 
 
 class FirestoreService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
 
   String? get _userId { 
     return _auth.currentUser?.uid;
@@ -71,4 +73,77 @@ class FirestoreService {
       );
     }
   }
+  Future<void> removeCollection(String collectionId) async {
+    if (_userId != null) {
+      QuerySnapshot booksSnapshot = await _firestore
+          .collection('users')
+          .doc(_userId)
+          .collection('collections')
+          .doc(collectionId)
+          .collection('books')
+          .get();
+      for (var doc in booksSnapshot.docs) {
+        await removeBook(collectionId, doc.id);
+      }
+      await _firestore
+          .collection('users')
+          .doc(_userId)
+          .collection('collections')
+          .doc(collectionId)
+          .delete();
+    } else {
+      print("Error: User not logged in while removing collection");
+    }
+  }
+
+ Future<void> removeBook(String collectionId, String bookId) async {
+  if (_userId == null) {
+    throw Exception("User not logged in");
+  }
+  
+  try {
+    DocumentSnapshot bookDoc = await _firestore
+        .collection('users')
+        .doc(_userId)
+        .collection('collections')
+        .doc(collectionId)
+        .collection('books')
+        .doc(bookId)
+        .get();
+
+    if (!bookDoc.exists) {
+      throw Exception("Book not found");
+    }
+
+    Map<String, dynamic> bookData = bookDoc.data() as Map<String, dynamic>;
+    String? imageUrl = bookData['imageUrl'] as String?;
+    if (imageUrl != null && imageUrl.isNotEmpty) {
+      try {
+        print("Attempting to delete image: $imageUrl");
+        Reference ref = FirebaseStorage.instance.refFromURL(imageUrl);
+        await ref.delete();
+        print("Book cover image deleted successfully: $imageUrl");
+      } catch (e) {
+        print("Error deleting book cover image: $e");
+       
+      }
+    } else {
+      print("No image URL found for the book");
+    }
+
+    await _firestore
+        .collection('users')
+        .doc(_userId)
+        .collection('collections')
+        .doc(collectionId)
+        .collection('books')
+        .doc(bookId)
+        .delete();
+
+    print("Book document deleted successfully");
+  } catch (e) {
+    print("Error removing book: $e");
+    throw Exception("Failed to remove book: $e");
+  }
+}
 }
